16a4a4e17505eec37b8cde8c988c9742a6e44d77
amigazen <amigazen@noreply.github.com>
2025-09-09 22:29:42 +0100

    Added smakefile Fixed refactored alib.c to ANSI C standards Fixed bumped version number to 2.0 Fixed rewrite of usage help text to POSIX standards

--- src/alib/alib.c
+++ src/alib/alib.c
@@ -21,8 +21,11 @@
  *       or commas.  Note that if .o is not present in the filenames,
  *       .o will be appended.
  */
-#include "stdio.h"
-#include "fcntl.h"
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 /*
  * The following structure defines the records in the .dir file for
@@ -47,14 +50,29 @@ struct   liblist_def    {
    };
 
 /*
- * external functions
+ * Function prototypes
  */
-extern   char  *getmem();
-extern   long  lseek();
+void help(void);
+int open_library(void);
+int kill_module(char *name);
+void add_module(char *name);
+void close_library(void);
+void directory(void);
+int main(int argc, char *argv[]);
+
+/*
+ * Memory management functions (Amiga-specific replacements)
+ */
+char *alib_getmem(size_t size);
+void alib_rlsmem(char *ptr, size_t size);
 
 /*
  * Global variables
  */
+
+static const char *verstag = "$VER: alib 2.0 (09.09.2025)\n";
+static const char *stack_cookie = "$STACK: 8192";
+
 LL    *objlist=NULL;                /* linked list of object modules */
 
 char  libname[80];                  /* name of library file */
@@ -70,21 +88,42 @@ char  option;                       /* user selected option */
 /*
  * This function prints the help messages and exits.
  */
-help() {
-   printf("Use:\n");
-   printf("   ALIB  option  library filelist \n");
-   printf("Where\n");
-   printf("   option may be one of the following:\n");
-   printf("      D  =  delete object modules from library\n");
-   printf("      R  =  replace object modules (or add if not defined) \n");
-   printf("      L  =  directory of library\n");
-   printf("   library is the name of the library file to be created or modified.\n");
-   printf("      if no .lib extension is provided on the command line, it is \n");
-   printf("      appended.  Also note that a file library.dir is created for\n");
-   printf("      each library file and is managed by this program. \n");
-   printf("   filelist is a list of object module filenames separated by spaces\n");
-   printf("      or commas.  Note that if .o is not present in the filenames, \n");
-   printf("      .o will be appended. \n");
+void help(void) {
+   printf("alib - Amiga Object Module Librarian\n");
+   printf("\n");
+   printf("SYNOPSIS\n");
+   printf("    alib d library [file ...]\n");
+   printf("    alib r library [file ...]\n");
+   printf("    alib l library\n");
+   printf("    alib --help\n");
+   printf("    alib --version\n");
+   printf("\n");
+   printf("DESCRIPTION\n");
+   printf("    The alib utility manages Amiga object module libraries. It can add,\n");
+   printf("    replace, or delete object modules from a library file.\n");
+   printf("\n");
+   printf("COMMANDS\n");
+   printf("    d    Delete object modules from library\n");
+   printf("    r    Replace object modules (or add if not defined)\n");
+   printf("    l    List directory of library\n");
+   printf("\n");
+   printf("OPTIONS\n");
+   printf("    --help     Display this help and exit\n");
+   printf("    --version  Display version information and exit\n");
+   printf("\n");
+   printf("OPERANDS\n");
+   printf("    library   The name of the library file to be created or modified.\n");
+   printf("              If no .lib extension is provided, it is appended.\n");
+   printf("              A file library.dir is created for each library file.\n");
+   printf("    file      Object module filenames. If .o is not present in the\n");
+   printf("              filename, .o will be appended.\n");
+   printf("\n");
+   printf("EXIT STATUS\n");
+   printf("    0    Successful completion\n");
+   printf("    1    Error occurred\n");
+   printf("\n");
+   printf("AUTHOR\n");
+   printf("    Written by Mike Schwartz (C) 1985, MS Software. Updated by amigazen project 2025.\n");
    exit(1);
    }
 
@@ -93,7 +132,7 @@ help() {
  * file does not exist, then the user is asked if he wants to create it.
  * If the library exists, then it is read in and built in a linked list.
  */
-open_library() {
+int open_library(void) {
    LL    *lp;           /* pointer to module node */
 
    /*
@@ -106,12 +145,13 @@ open_library() {
        */
       while (1) {
          printf("%s does not exist, create it? (Y/N): ", libname);
-         gets(buf);
-         if (buf[0] == 'y' || buf[0] == 'Y')
-            return !0;
-         if (buf[0] == 'n' || buf[0] == 'N') {
-            printf("Alib abandoned\n");
-            exit(1);
+         if (fgets(buf, sizeof(buf), stdin) != NULL) {
+            if (buf[0] == 'y' || buf[0] == 'Y')
+               return 1;
+            if (buf[0] == 'n' || buf[0] == 'N') {
+               printf("Alib abandoned\n");
+               exit(1);
+               }
             }
          }
       }
@@ -128,7 +168,7 @@ open_library() {
       /*
        * Allocate a node to hold the object module.
        */
-      lp = (LL *)getmem(sizeof(LL));
+      lp = (LL *)alib_getmem(sizeof(LL));
       if (lp == NULL) {
          printf("Not enough memory.\nAlib abandoned\n");
          exit(1);
@@ -141,7 +181,7 @@ open_library() {
       /*
        * Allocate a buffer to hold the object file image.
        */
-      lp->object_module = getmem(lp->dir_entry.module_size);
+      lp->object_module = alib_getmem(lp->dir_entry.module_size);
       if (lp->object_module == NULL) {
          printf("Not enough memory.\nAlib abandoned\n");
          exit(1);
@@ -166,14 +206,14 @@ open_library() {
    close(libfd);
    close(dirfd);
    printf("Library file read in ok\n");
+   return 0;
    }
 
 /*
  * The following function removes an object module from the linked list
  * and frees up any memory used by it.
  */
-kill_module(name)
-char  *name;
+int kill_module(char *name)
 {
    LL    *lp;
    LL    *lp2;
@@ -184,8 +224,8 @@ char  *name;
    strcat(objname, ".o");
    if (strcmp(objlist->dir_entry.object_filename,  objname) == 0) {
       lp = objlist->next;
-      rlsmem(objlist->object_module, objlist->dir_entry.module_size);
-      rlsmem((char *)objlist, sizeof(LL));
+         alib_rlsmem(objlist->object_module, objlist->dir_entry.module_size);
+         alib_rlsmem((char *)objlist, sizeof(LL));
       objlist = lp;
       return !0;
       }
@@ -193,21 +233,19 @@ char  *name;
       if (strcmp(lp->next->dir_entry.object_filename, objname) == 0) {
          lp2 = lp->next;
          lp->next = lp2->next;
-         rlsmem(lp2->object_module, lp2->dir_entry.module_size);
-         rlsmem((char *)lp2, sizeof(LL));
+         alib_rlsmem(lp2->object_module, lp2->dir_entry.module_size);
+         alib_rlsmem((char *)lp2, sizeof(LL));
          return !0;
          }
    return 0;
    }
 
 /*
- * The following function
+ * The following function adds a module to the library
  */
-add_module(name)
-char  *name;
+void add_module(char *name)
 {
    LL    *lp;
-   int   len;
 
    strcpy(objname, name);
    strcat(objname, ".o");
@@ -224,7 +262,7 @@ printf("adding %s\n", objname);
    /*
     * Allocate a node to hold the file.
     */
-   lp = (LL *)getmem(sizeof(LL));
+   lp = (LL *)alib_getmem(sizeof(LL));
    if (lp == NULL) {
       printf("Not enough memory.\nAlib abandoned\n");
       exit(1);
@@ -233,13 +271,13 @@ printf("adding %s\n", objname);
     * Initialize it.
     */
    strcpy(lp->dir_entry.object_filename, objname);
-   lp->dir_entry.module_size = lseek(objfd, 0, 2);
-printf("module size = %d\n", lp->dir_entry.module_size);
-   lseek(objfd, 0, 0);
+   lp->dir_entry.module_size = (long)lseek(objfd, 0, SEEK_END);
+printf("module size = %ld\n", lp->dir_entry.module_size);
+   lseek(objfd, 0, SEEK_SET);
    /*
     * Allocate the buffer to read the object module into.
     */
-   lp->object_module = getmem(lp->dir_entry.module_size);
+   lp->object_module = alib_getmem(lp->dir_entry.module_size);
    if (lp->object_module == NULL) {
       printf("Not enough memory.\nAlib abandoned\n");
       exit(1);
@@ -267,7 +305,7 @@ printf("module size = %d\n", lp->dir_entry.module_size);
 /*
  * The following routine writes the library from memory and exits.
  */
-close_library() {
+void close_library(void) {
    LL    *lp;
    long  file_position;
    long  length;
@@ -315,16 +353,9 @@ close_library() {
 /*
  * Main program.
  */
-main(argc, argv)
-int   argc;
-char  *argv[];
+int main(int argc, char *argv[])
 {
    int   count;
-
-   printf("ALIB Rev. 1.0 \n");
-   printf("Amiga Object Module Librarian\n");
-   printf("Programmed by Mike Schwartz\n");
-   printf("(C)1985 MS Software, all rights reserved\n");
 
    /*
     * check for command line parameters present.
@@ -334,7 +365,17 @@ char  *argv[];
    /*
     * setup the option and filenames
     */
-   option = argv[1][0];
+   if (strcmp(argv[1], "--help") == 0) {
+      help();
+   } else if (strcmp(argv[1], "--version") == 0) {
+      printf("alib 2.0\n");
+      printf("Amiga Object Module Librarian\n");
+      exit(0);
+   } else if (strlen(argv[1]) == 1) {
+      option = argv[1][0];  /* Single character command: d, r, l */
+   } else {
+      help();  /* Invalid command format */
+   }
    strcpy(libname, argv[2]);
    strcpy(dirname, argv[2]);
    strcat(libname, ".lib");
@@ -367,9 +408,10 @@ char  *argv[];
          }
       }
    close_library();
+   return 0;
    }
 
-directory() {
+void directory(void) {
    LD    d_entry;
    int   count;
    int   bytes;
@@ -383,12 +425,27 @@ directory() {
    printf("Module Name                    Size   Offset\n");
    count = bytes = 0;
    while (read(dirfd, (char *)&d_entry, sizeof(LD)) == sizeof(LD)) {
-      printf("%-30.30s %-6d %-6d\n", d_entry.object_filename, 
+      printf("%-30.30s %-6ld %-6ld\n", d_entry.object_filename, 
                d_entry.module_size, d_entry.module_offset);
       count++;
       bytes += d_entry.module_size;
       }
-   printf("Library consists of %d entries totalling %d bytes\n", count,
+   printf("Library consists of %d entries totalling %ld bytes\n", count,
             bytes);
    close(dirfd);
    }
+
+/*
+ * Memory management function implementations
+ * These replace Amiga-specific memory functions with standard C equivalents
+ */
+char *alib_getmem(size_t size)
+{
+   return malloc(size);
+}
+
+void alib_rlsmem(char *ptr, size_t size)
+{
+   (void)size; /* size parameter not used in free() but kept for compatibility */
+   free(ptr);
+}
